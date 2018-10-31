d2e0df9278107725305523dd7bb36a69ae9f938c
edwardvini123 <42192100+edwardvini123@users.noreply.github.com>
2018-10-30 21:29:09 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,6 +15,4 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-    
-class Vestibular(models.Model):
-    nome = models.CharField(max_length=200)
+   
